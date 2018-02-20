32250acda5fec9510071d35c4d29f70fa39004e8
Kamil Kowalski <kowalski.kamil90@gmail.com>
2018-02-20 19:01:58 +0100

    added exemplary note file, added command line arg for note file

--- note_maker.cc
+++ note_maker.cc
@@ -29,17 +29,23 @@
  */
 
 #include <iostream>
+#include <cstring>
+
+using namespace std;
 
 int main(int argc, char**argv) {
     // Prints welcome message...
-    std::cout << "Welcome ..." << std::endl;
+    std::cout << "Welcome to the note maker..." << std::endl;
+    std::cout << "author: Kamil Kowalski" << std::endl << std::endl;
 
-    // Prints arguments...
     if (argc > 1) {
-        std::cout << std::endl << "Arguments:" << std::endl;
-        for (int i = 1; i < argc; i++) {
-            std::cout << i << ": " << argv[i] << std::endl;
-        }
+
+	std::string notes_file(argv[1]);
+	std::cout << "Following note file was chosen::" << notes_file << std::endl;
+    }
+    else {
+        std::cout << "Please specify the NOTES file when running the program." << std::endl;
+	std::cout << "E.g: " << argv[0] << " <notes_file_path> " << std::endl;
     }
     
     return 0;
